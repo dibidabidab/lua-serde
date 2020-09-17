99450ac41278358e82293116f42f4a5cc9d8e839
Niek Janssen <niek.janssen01@gmail.com>
2020-09-17 22:15:46 +0200

    reverted single-file refactor

---
+++ load.py
@@ -0,0 +1,68 @@
+# Files
+from os import path
+from glob import glob
+
+import yaml
+
+
+def readGlobAST(configGlob):
+    paths = glob(configGlob)
+    return [readAST(path) for path in paths]
+
+
+def readAST(configPath):
+    file = open(configPath)
+    structs = yaml.load(file, Loader=yaml.FullLoader)
+    basepath, _ = path.splitext(configPath)
+    return prepareAST(structs, basepath)
+
+
+# Prepare for generation
+def popOr(dict, key, default):
+    if key in dict:
+        return dict.pop(key)
+    return default
+
+
+def prepareAST(structs, basepath):
+    config = popOr(structs, 'config', {})
+    for key in structs:
+        structs[key] = prepareStruct(structs[key])
+    config['pragma_once'] = basepath.replace('.', '_').replace('/', '_')
+
+    return {
+        'config': config,
+        'structs': structs,
+        'filename': path.basename(basepath),
+    }
+
+
+def prepareStruct(struct):
+    cpp_only = popOr(struct, '_cpp_only', {})
+    methods = popOr(struct, '_methods', [])
+    hash = popOr(struct, '_hash', [])
+
+    return {
+        'expose': prepareVars(struct),
+        'cpp_only': prepareVars(cpp_only),
+        'methods': methods,
+        'hash': hash
+    }
+
+
+def prepareVars(dict):
+    prepared = []
+    for key, value in dict.items():
+        if type(value) is list:
+            typ, default = value
+            prepared.append({
+                'name': key,
+                'typ': typ,
+                'default': default,
+            })
+        else:
+            prepared.append({
+                'name': key,
+                'typ': value,
+            })
+    return prepared
---
+++ template.py
@@ -0,0 +1,17 @@
+# Rendering
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=False,
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
+template = env.get_template('struct.jinja')
+
+
+def renderStructs(ast):
+    print(ast)
+    return template.render(structs=ast['structs'], config=ast['config'])
+
