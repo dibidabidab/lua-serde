489cfec322097657017fef72a63295429dd15f8d
Niek Janssen <niek.janssen01@gmail.com>
2020-09-17 21:02:17 +0200

    Struct and info generation works, serde has to be added

--- load.py
+++ load.py
@@ -1,15 +1,61 @@
 import glob
 import yaml
+import os
 
 
-def readGlobStructs(configGlob):
+def readGlobAST(configGlob):
     paths = glob.glob(configGlob)
-    structs = [readStructs(path) for path in paths]
-    return [struct for struct1d in structs for struct in struct1d]
+    return [readAST(path) for path in paths]
 
 
-def readStructs(path):
+def readAST(path):
     file = open(path)
     structs = yaml.load(file, Loader=yaml.FullLoader)
-    print(structs)
-    return structs
+    return prepareAST(structs, os.path.basename(path))
+
+
+def popOr(dict, key, default):
+    if key in dict:
+        return dict.pop(key)
+    return default
+
+
+def prepareAST(structs, filename):
+    config = popOr(structs, 'config', {})
+    for key in structs:
+        structs[key] = prepareStruct(structs[key])
+
+    return {
+        'config': config,
+        'structs': structs,
+        'filename': filename,
+    }
+
+
+def prepareStruct(struct):
+    cpp_only = popOr(struct, '_cpp_only', {})
+    methods = popOr(struct, '_methods', [])
+
+    return {
+        'expose': prepareVars(struct),
+        'cpp_only': prepareVars(cpp_only),
+        'methods': methods,
+    }
+
+
+def prepareVars(dict):
+    prepared = []
+    for key, value in dict.items():
+        if type(value) is list:
+            typ, default = value
+            prepared.append({
+                'name': key,
+                'typ': typ,
+                'default': default,
+            })
+        else:
+            prepared.append({
+                'name': key,
+                'typ': value,
+            })
+    return prepared
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #! /bin/python
 
 import sys
+import os
 
-import load
+from load import readGlobAST
+from template import renderStructs
 
 if len(sys.argv) != 3:
     print("Usage: ")
@@ -12,4 +14,23 @@ if len(sys.argv) != 3:
 configGlob = sys.argv[1]
 outputDir = sys.argv[2]
 
-structures = load.readGlobStructs(configGlob)
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
+astList = readGlobAST(configGlob)
+generatedFiles = []
+
+for ast in astList:
+    render = renderStructs(ast)
+
+    basename, ext = os.path.splitext(ast['filename'])
+    filename = basename + '.hpp'
+    generatedFiles.append(filename)
+
+    file = open(os.path.join(outputDir, filename), 'w')
+    file.write(render)
+    file.close()
+
+file = open(os.path.join(outputDir, 'include_all.hpp'), 'w')
+file.writelines(['#include "' + filename + '"' for filename in generatedFiles])
+file.close()
